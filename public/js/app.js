879d847b55037d7113656e46ed1341e512cf97ac
mckay <mckay.db@gmail.com>
2015-08-09 10:01:57 -0600

    Added in some fade out text effects and jquery animations to make the hide and show a little more pretty

--- public/js/app.js
+++ public/js/app.js
@@ -2,12 +2,21 @@ $(".more_details").on("click", function() {
     var honoreeInformation = $(this).siblings(".honoree_information");
     var overflow = honoreeInformation.css("overflow");
     if ( overflow === "hidden" ) {
-        honoreeInformation.css("overflow", "visible");
-        honoreeInformation.css("height", "auto");
+        honoreeInformation.css({
+            overflow: "visible",
+        }).animate({
+            height: honoreeInformation[0].scrollHeight
+        }, 500);
+        honoreeInformation.addClass( "no_after" );
         $(this).text("Hide Details");
     } else if ( overflow === "visible" ) {
-        honoreeInformation.css("overflow", "hidden");
-        honoreeInformation.css("height", "250px");
+        honoreeInformation.css({
+            overflow: "hidden"
+        }).animate({
+            height: "250px"
+        }, 500);
+
+        honoreeInformation.removeClass( "no_after" );
         $(this).text("Show Details");
     }
 });
